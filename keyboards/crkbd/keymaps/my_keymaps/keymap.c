100ec886b85eacbfdd6a68831e2f7accd4b47b34
Kay Gosho <ketsume0211@gmail.com>
2022-10-12 00:55:44 +0900

    swap btn1 and gui key

--- keyboards/crkbd/keymaps/my_keymaps/keymap.c
+++ keyboards/crkbd/keymaps/my_keymaps/keymap.c
@@ -24,11 +24,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_TAB,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,LT(1, KC_A), KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, LT(1, KC_SCLN), MT(MOD_RCTL, KC_QUOT),
+      KC_LCTL,LT(1, KC_A), KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, LT(1, KC_SCLN), KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,LT(2, KC_Z), KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, LT(2, KC_SLSH), KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, KC_LALT,  KC_SPC,     KC_ENT, KC_RALT, KC_BTN1
+                                          KC_BTN1, KC_LALT,  KC_SPC,     KC_ENT, KC_RALT, KC_LGUI
                                       //`--------------------------'  `--------------------------'
 
   ),

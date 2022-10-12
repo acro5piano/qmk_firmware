78c3a9692316242af5bfb0e82c86550c52c272d1
Kay Gosho <ketsume0211@gmail.com>
2022-10-12 18:18:50 +0900

    restore the right ctrl key

--- keyboards/crkbd/keymaps/my_keymaps/keymap.c
+++ keyboards/crkbd/keymaps/my_keymaps/keymap.c
@@ -24,11 +24,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_TAB,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  MT(MOD_RCTL, KC_QUOT),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,LT(1, KC_Z), KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, LT(2, KC_SLSH), KC_RSFT,
+      KC_LSFT,LT(1, KC_Z), KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, LT(1, KC_SLSH), KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, KC_LALT,  KC_SPC,     KC_ENT, KC_RALT,    MO(1)
+                                          KC_LGUI, KC_LALT,  KC_SPC,     KC_ENT, KC_RALT,    MO(2)
                                       //`--------------------------'  `--------------------------'
 
   ),
